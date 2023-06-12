376983f54135f4829857dd6fa8b85da074e337a4
kimhyerin0909 <khyerin0909@gmail.com>
2023-06-12 19:33:38 +0900

    Add - login 퍼블리싱

--- lib/main.dart
+++ lib/main.dart
@@ -14,55 +14,88 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      backgroundColor: colors.white,
-      body: Center(
-          child: Wrap(
-        runSpacing: 20,
-        alignment: WrapAlignment.center,
-        children: [
-          Center(
-            child: SvgPicture.asset(
-              "lib/assets/logo.svg",
-              width: 40,
-              height: 40,
-            ),
-          ),
-          Text(
-            "오늘 1일 1커밋 했나?!",
-            style: TextStyle(
-              fontSize: 14,
-              color: colors.baseBlack,
-            ),
-          ),
-          FractionallySizedBox(
-            widthFactor: 1,
-            child: Column(
+        appBar: null,
+        backgroundColor: colors.white,
+        body: Stack(
+          children: [
+            Center(
+                child: Wrap(
+              runSpacing: 20,
+              alignment: WrapAlignment.center,
               children: [
-                Container(
-                  color: colors.black,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Wrap(children: [
-                      SvgPicture.asset("lib/assets/github.svg"),
-                      Text(
-                        "Github로 로그인",
-                        style: TextStyle(color: colors.white),
-                      )
-                    ]),
+                Center(
+                  child: SvgPicture.asset(
+                    "lib/assets/logo.svg",
+                    width: 40,
+                    height: 40,
                   ),
                 ),
-                Wrap(
-                  children: [
-                    SvgPicture.asset("lib/assets/apple.svg"),
-                    Text("Apple로 로그인")
-                  ],
+                Text(
+                  "오늘 1일 1커밋 했나?!",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: colors.baseBlack,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Wrap(
+                    runSpacing: 10,
+                    alignment: WrapAlignment.center,
+                    children: [
+                      SocialLogin(
+                        socialType: "github",
+                      ),
+                      SocialLogin(
+                        socialType: "apple",
+                      ),
+                    ],
+                  ),
                 ),
               ],
+            )),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image(
+                image: AssetImage("lib/assets/backImg.png"),
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class SocialLogin extends StatelessWidget {
+  const SocialLogin({super.key, this.socialType});
+
+  final socialType;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: colors.black,
             ),
-          )
-        ],
-      )),
+            color: socialType == "github" ? colors.black : colors.white,
+            borderRadius: BorderRadius.circular(150)),
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Wrap(spacing: 60, children: [
+            SvgPicture.asset("lib/assets/$socialType.svg"),
+            Text(
+              "$socialType로 로그인",
+              style: TextStyle(
+                  color: socialType == "github" ? colors.white : colors.black),
+            ),
+            SizedBox()
+          ]),
+        ),
+      ),
     );
   }
 }
